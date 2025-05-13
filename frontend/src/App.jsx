0a9fcc1d9992eec3a019b4ad61b261bdcb8681ac
Alec Faustino <alecfaustino@outlook.com>
2025-05-13 07:21:11 -0600

    initiate footer component and style sheet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navigation />
       <h1>Hello World</h1>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/Footer.scss";
+const Footer = () => {
+  return (
+    <>
+      <div className="footer_main_container">
+        <ul>
+          <li className="footer_contact_logos">1</li>
+          <li className="footer_contact_logos">1</li>
+          <li className="footer_contact_logos">1</li>
+          <li className="footer_contact_logos">1</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
