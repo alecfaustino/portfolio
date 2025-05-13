102c57eecd0c37d5b1ca284cbeab6574b33400c0
Alec Faustino <alecfaustino@outlook.com>
2025-05-12 22:19:27 -0600

    apply display flex to the left side of the navbar

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -4,10 +4,13 @@ import "../styles/Navigation.scss";
 const Navigation = () => {
   return (
     <>
-      <div>
+      <div className="nav_container">
         <div className="nav_branding">
-          <p>temp</p>
-          <h2 className="nav_name">Alec Faustino</h2>
+          <img
+            className="nav_img"
+            src="../../public/compressed_image_new.jpg"
+            alt="small photo of Alec Faustino"></img>
+          <span className="nav_name">Alec Faustino</span>
         </div>
       </div>
     </>
