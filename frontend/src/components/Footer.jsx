61014b00794da60ad4e79753e94401e0d62786e9
Alec Faustino <alecfaustino@outlook.com>
2025-05-13 07:34:46 -0600

    add unstyled icons

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import "../styles/Footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
   return (
     <>
       <div className="footer_main_container">
-        <ul>
-          <li className="footer_contact_logos">1</li>
-          <li className="footer_contact_logos">1</li>
-          <li className="footer_contact_logos">1</li>
-          <li className="footer_contact_logos">1</li>
-        </ul>
+        <div className="footer_logo_container">
+          <ul>
+            <li className="footer_contact_logos">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </li>
+            <li className="footer_contact_logos">
+              <FontAwesomeIcon icon={faPhone} size="2x" />
+            </li>
+            <li className="footer_contact_logos">
+              <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            </li>
+            <li className="footer_contact_logos">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
