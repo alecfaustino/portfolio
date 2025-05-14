20d0b5a5528a98b2814f578b5a0afbc2aad8740f
Alec Faustino <alecfaustino@outlook.com>
2025-05-14 17:50:31 -0600

    add the onClick to the left side nav too

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -5,7 +5,7 @@ const Navigation = ({ selectTopic, topic }) => {
   return (
     <>
       <div className="nav_container">
-        <div className="nav_branding">
+        <div className="nav_branding" onClick={() => selectTopic("landing")}>
           <img
             className="nav_img"
             src="../../public/compressed_image_new.jpg"
@@ -14,41 +14,11 @@ const Navigation = ({ selectTopic, topic }) => {
         </div>
         <div className="nav_topics_container">
           <ul>
-            <li
-              onClick={() => {
-                selectTopic("landing");
-                console.log(topic);
-              }}>
-              Home
-            </li>
-            <li
-              onClick={() => {
-                selectTopic("projects");
-                console.log(topic);
-              }}>
-              Projects
-            </li>
-            <li
-              onClick={() => {
-                selectTopic("experience");
-                console.log(topic);
-              }}>
-              Experience
-            </li>
-            <li
-              onClick={() => {
-                selectTopic("interests");
-                console.log(topic);
-              }}>
-              Interests
-            </li>
-            <li
-              onClick={() => {
-                selectTopic("contact");
-                console.log(topic);
-              }}>
-              Contact
-            </li>
+            <li onClick={() => selectTopic("landing")}>Home</li>
+            <li onClick={() => selectTopic("projects")}>Projects</li>
+            <li onClick={() => selectTopic("experience")}>Experience</li>
+            <li onClick={() => selectTopic("interests")}>Interests</li>
+            <li onClick={() => selectTopic("contact")}>Contact</li>
           </ul>
         </div>
       </div>
