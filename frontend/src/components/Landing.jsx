1844e717b95ec24ecacabce16daab3b5e933f1f7
Alec Faustino <alecfaustino@outlook.com>
2025-05-14 07:35:01 -0600

    style image and about me

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -7,6 +7,19 @@ const Landing = () => {
         <div>
           <img className="landing_main_img" src="../../public/mainphoto.jpg" />
         </div>
+        <div className="landing_interactive_container">
+          <div className="landing_Aboutme_container">
+            <h1>aboutMe</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ut
+              omnis, iure quidem totam, quos quae quia explicabo corrupti
+              tenetur libero aliquid voluptas ullam minus non, culpa sit
+              nesciunt. Earum temporibus obcaecati iure amet eligendi est
+              aperiam natus. Nihil, a quod hic quisquam excepturi necessitatibus
+              soluta. Quas repellendus quos culpa!
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
