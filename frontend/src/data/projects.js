b0b1ec591268cf3e5ab5510e0c94a18e87d99026
Alec Faustino <alecfaustino@outlook.com>
2025-05-15 07:44:39 -0600

    design projects data structure

---
+++ frontend/src/data/projects.js
@@ -0,0 +1,11 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Some App I'll add later",
+    stack: ["React. Node.js, ExpressjS"],
+    deployedUrl: "Not live, code only",
+    repoURL: "https://...",
+    isDeployed: false, 
+    highlights: [""],
+  },
+];
