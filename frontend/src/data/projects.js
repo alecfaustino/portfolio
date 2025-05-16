7356edde14dc8c462a25bd18ae7c6578a3056252
Alec Faustino <alecfaustino@outlook.com>
2025-05-15 18:55:51 -0600

    add some mock project data to test JSX

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -1,39 +1,43 @@
 const projectsList = [
   {
     id: 1,
-    title: "Some App I'll add later",
-    stack: ["React. Node.js, ExpressjS"],
-    deployedUrl: "Not live, code only",
-    repoURL: "https://...",
-    isDeployed: false, 
-    highlights: [""],
+    title: "The Kid's Place Daycare",
+    stack: ["HTML", "CSS", "JavaScript"],
+    deployedUrl: "https://thekidsplacedaycare.com/",
+    repoURL: "Private Repo",
+    isDeployed: true, 
+    highlights: ["This project was intended to be for me to learn and explore the basic vanilla HTML, CSS, and JavaScript. I ended up learning key-skills like mobile-first development and responsive design. This was created with no external libraries or frameworks. I Collaborated directly with the client to translate their vision into a site that highlighted key services and encouraged lead generation."],
+    photo: "/thekidsplacedaycare.png"
   },
   {
     id: 2,
     title: "Some App I'll add later",
-    stack: ["React. Node.js, ExpressjS"],
+    stack: ["React", "Node.js", "ExpressjS"],
     deployedUrl: "Not live, code only",
     repoURL: "https://...",
     isDeployed: false, 
     highlights: [""],
+    photo: "/placeholder.png"
   },
   {
     id: 3,
     title: "Some App I'll add later",
-    stack: ["React. Node.js, ExpressjS"],
+    stack: ["React", "Node.js", "ExpressjS"],
     deployedUrl: "Not live, code only",
     repoURL: "https://...",
     isDeployed: false, 
     highlights: [""],
+    photo: "/placeholder.png"
   },
   {
     id: 4,
     title: "Some App I'll add later",
-    stack: ["React. Node.js, ExpressjS"],
+    stack: ["React", "Node.js", "ExpressjS"],
     deployedUrl: "Not live, code only",
     repoURL: "https://...",
     isDeployed: false, 
     highlights: [""],
+    photo: "/placeholder.png"
   },
 ];
 
