f6f1d1373395f82fa42956f0247aa1d9e3673100
Alec Faustino <alecfaustino@outlook.com>
2025-05-17 22:03:40 -0600

    change to hamburger on smaller media

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navigation.scss";
 
-const Navigation = ({ selectTopic, topic }) => {
+const Navigation = ({ selectTopic }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleNavClick = (topic) => {
+    selectTopic(topic);
+    setMenuOpen(false); // Close menu on navigation
+  };
+
   return (
-    <>
-      <div className="nav_container">
-        <div className="nav_branding" onClick={() => selectTopic("landing")}>
-          <img
-            className="nav_img"
-            src="/compressed_image_new.jpg"
-            alt="small photo of Alec Faustino"></img>
-          <h2 className="nav_name">Alec Faustino</h2>
-        </div>
-        <div className="nav_topics_container">
-          <ul>
-            <li onClick={() => selectTopic("landing")}>Home</li>
-            <li onClick={() => selectTopic("projects")}>Projects</li>
-            <li onClick={() => selectTopic("experience")}>Experience</li>
-            <li onClick={() => selectTopic("contact")}>Contact</li>
-          </ul>
-        </div>
+    <div className="nav_container">
+      <div className="nav_branding" onClick={() => handleNavClick("landing")}>
+        <img
+          className="nav_img"
+          src="/compressed_image_new.jpg"
+          alt="small photo of Alec Faustino"
+        />
+        <h2 className="nav_name">Alec Faustino</h2>
       </div>
-    </>
+
+      <div className="hamburger" onClick={() => setMenuOpen((prev) => !prev)}>
+        <div className="bar" />
+        <div className="bar" />
+        <div className="bar" />
+      </div>
+
+      <div className={`nav_topics_container ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <li onClick={() => handleNavClick("landing")}>Home</li>
+          <li onClick={() => handleNavClick("projects")}>Projects</li>
+          <li onClick={() => handleNavClick("experience")}>Experience</li>
+          <li onClick={() => handleNavClick("contact")}>Contact</li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
