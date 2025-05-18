4d654f1dc52737be6c76bb87852b10990ca25e27
Alec Faustino <alecfaustino@outlook.com>
2025-05-18 11:11:15 -0600

    create a unique id for visitors

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cookieParser());
 
-app.use(cookieSession({
-  name: 'session',
-  keys: [],
-}));
+const generateRandomString = function() {
+  return Math.random()
+    .toString(36)
+    .substring(2, 2 + 6);
+};
+app.use('/', function (req, res, next) {
+  if (!req.cookies.visitor_id) {
+    const randomString = generateRandomString();
+    res.cookie('visitor_id', randomString, {
+      maxAge: 1000 * 60 * 60 * 24 * 3650, httpOnly: true, 
+    });
+  }
+
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Server is live on port ${port}`);
