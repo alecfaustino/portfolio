cbfe02e8549856820523832967467356ca5d8cea
Alec Faustino <alecfaustino@outlook.com>
2025-09-03 09:54:12 -0600

    move techicons into a helper file

--- frontend/src/components/ProjectModal.jsx
+++ frontend/src/components/ProjectModal.jsx
@@ -1,46 +1,7 @@
 import React from "react";
 import "../styles/ProjectModal.scss";
-import {
-  SiJavascript,
-  SiRuby,
-  SiHtml5,
-  SiCss3,
-  SiReact,
-  SiNodedotjs,
-  SiExpress,
-  SiRubyonrails,
-  SiJquery,
-  SiBootstrap,
-  SiSass,
-  SiMongodb,
-  SiPostgresql,
-  SiGit,
-  SiMocha,
-} from "react-icons/si";
-import { FaCookie, FaLock, FaMugHot, FaExchangeAlt } from "react-icons/fa";
+import techIcons from "../data/techicons";
 import ImageSlider from "./ImageSlider";
-
-const techIcons = {
-  html: <SiHtml5 title="HTML" />,
-  css: <SiCss3 title="CSS" />,
-  javascript: <SiJavascript title="JavaScript" />,
-  react: <SiReact title="React" />,
-  nodejs: <SiNodedotjs title="Node.js" />,
-  expressjs: <SiExpress title="Express.js" />,
-  ruby: <SiRuby title="Ruby" />,
-  jquery: <SiJquery title="jQuery" />,
-  cookies: <FaCookie title="cookies" />,
-  bcrypt: <FaLock title="bcrypt" />,
-  ajax: <FaExchangeAlt title="AJAX" />,
-  bootstrap: <SiBootstrap title="bootstrap" />,
-  sass: <SiSass title="sass" />,
-  postgresql: <SiPostgresql title="PostgreSQL" />,
-  mocha: <SiMocha title="mocha" />,
-  git: <SiGit title="git" />,
-  chai: <FaMugHot title="chai" />,
-  mongodb: <SiMongodb title="MongoDB" />,
-  rails: <SiRubyonrails title="Ruby on Rails" />,
-};
 
 const ProjectModal = ({ project, setShowModal }) => {
   if (!project) return null;
